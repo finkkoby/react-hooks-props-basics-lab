27f465e1753cfe778036c533d4e95896ee43bb2b
Koby Fink <finkkoby@icloud.com>
2024-02-20 14:40:25 -0500

    complete lab

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,20 @@
 import React from "react";
+import Links from './Links.js';
 
-function About() {
+function About(props) {
+  function testBio() {
+    if (props.bio) {
+      return (
+        <p>{props.bio}</p>
+      )
+    }
+  }
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {testBio()}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github={props.links.github} linkedin={props.links.linkedin}/>
     </div>
   );
 }
